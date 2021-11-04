77300a18aec71bd0f9a48e06c83558f1ed07a119
Andrew Pobrica <pirit66@gmail.com>
2021-11-04 00:29:06 -0400

    Added class for hoare, added makefile, added gitignore

---
+++ hoare.cpp
@@ -0,0 +1,57 @@
+//
+// Created by andrewiii on 11/3/21.
+//
+
+#include "hoare.h"
+
+    void sortF(int *data, int first, int last, int (*v) (int*, int, int)) {
+        if(first < last){
+            int pv = v(data, first, last);
+            sortF(data, first, pv-1, v);
+            sortF(data, pv+1, last, v);
+        }
+    }
+
+    void swap(int &a, int &b) {
+        int c=a;
+        a=b;
+        b=c;
+    }
+
+    int Hoare(int* data, int first, int last) {
+        if (first < last) {
+            int pivot = data[first];
+            int i = first;
+            int j = last + 1;
+            while (true) {
+                while (++i <= last && data[i] < pivot);
+                while (data[--j] > pivot);
+                if (i > j)
+                    break;
+                swap(data[i], data[j]);
+            }
+            swap(data[first], data[j]);
+            return j;
+        }
+        return -1;
+    }
+
+    int MHoare(int* data, int first, int last){
+        if(data[first]>data[last])
+            swap(data[first], data[last]);
+        int pivot = data[first];
+        while(true) {
+            while(data[--last] > pivot);
+            data[first]=data[last];
+            while(data[++first] < pivot);
+            if(first < last)
+                data[last]=data[first];
+            else{
+                if (data[last+1] <= pivot)
+                    last++;
+                data[last] = pivot;
+                return last;
+            }
+        }
+    }
+
---
+++ hoare.h
@@ -0,0 +1,20 @@
+//
+// Created by andrewiii on 11/3/21.
+//
+
+#ifndef HYBRID_QUICKSORT_HOARE_H
+#define HYBRID_QUICKSORT_HOARE_H
+
+class hoare {
+private:
+    void sortF(int *data, int first, int last, int (*v) (int*, int, int));
+        void swap(int &a, int &b);
+public:
+        int Hoare(int* data, int first, int last);
+
+        int MHoare(int* data, int first, int last);
+    };
+
+
+
+#endif //HYBRID_QUICKSORT_HOARE_H
