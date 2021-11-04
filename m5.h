e48345ff268f84d5f39a4b41bf5d0c7c3549fff9
Andrew Pobrica <pirit66@gmail.com>
2021-11-04 01:22:52 -0400

    Added m5 but its broken, variable change mid function is sus

---
+++ m5.h
@@ -0,0 +1,17 @@
+//
+// Created by andrewiii on 11/4/21.
+//
+
+#ifndef HYBRID_QUICKSORT_M5_H
+#define HYBRID_QUICKSORT_M5_H
+
+
+class m5 {
+private:
+    void swap(int &a, int &b);
+public:
+    int M5(int *data, int first, int last);
+};
+
+
+#endif //HYBRID_QUICKSORT_M5_H
